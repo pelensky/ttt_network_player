2c4b9a3008e02792f773cd40b1a73a6e11d6ab7d
Dan Pelensky <dan@pelensky.com>
2017-07-11 17:06:07 +0100

    added coveralls

--- project.clj
+++ project.clj
@@ -6,5 +6,5 @@
   :main tic-tac-toe.core
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:dev {:dependencies [[speclj "3.3.2"]]}}
-  :plugins [[speclj "3.3.2"]]
+  :plugins [[speclj "3.3.2"] [lein-cloverage "1.0.9"]]
   :test-paths ["spec"])
