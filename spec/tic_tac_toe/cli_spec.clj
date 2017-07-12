3df6ad742a8f7df9b1e05b164ce7660500cf4e4e
Dan Pelensky <dan@pelensky.com>
2017-07-12 08:58:33 +0100

    welcomes user

--- spec/tic_tac_toe/cli_spec.clj
+++ spec/tic_tac_toe/cli_spec.clj
@@ -1,3 +1,8 @@
 (ns tic-tac-toe.cli-spec
   (:require [speclj.core :refer :all]
             [tic-tac-toe.cli :refer :all]))
+
+(describe "CLI"
+  (it "Welcomes the user"
+    (should= "Tic Tac Toe\n"
+      (with-out-str (welcome)))))
--- src/tic_tac_toe/cli.clj
+++ src/tic_tac_toe/cli.clj
@@ -1,1 +1,4 @@
 (ns tic-tac-toe.cli)
+
+(defn welcome []
+  (println "Tic Tac Toe"))
