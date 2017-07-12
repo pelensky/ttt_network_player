7bbe8272ba68047ccbea68cdc9cea3d37e1b4521
Dan Pelensky <dan@pelensky.com>
2017-07-12 17:03:29 +0100

    created an app runner namespace

---
+++ spec/tic_tac_toe/app_runner_spec.clj
@@ -0,0 +1,3 @@
+(ns tic-tac-toe.app-runner-spec
+  (:require [speclj.core :refer :all]
+            [tic-tac-toe.app-runner :refer :all]))
---
+++ src/tic_tac_toe/app_runner.clj
@@ -0,0 +1,4 @@
+(ns tic-tac-toe.app-runner
+  (:require [tic-tac-toe.board :as ttt-board]
+            [tic-tac-toe.input :as ttt-input]
+            [tic-tac-toe.output :as ttt-output]))
