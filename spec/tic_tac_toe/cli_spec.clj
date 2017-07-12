4dc2bd9525b6fbb66d55053326fb4849e09cb7f6
Dan Pelensky <dan@pelensky.com>
2017-07-12 09:09:27 +0100

    prints empty board

--- spec/tic_tac_toe/cli_spec.clj
+++ spec/tic_tac_toe/cli_spec.clj
@@ -5,4 +5,10 @@
 (describe "CLI"
   (it "Welcomes the user"
     (should= "Tic Tac Toe\n"
-      (with-out-str (welcome)))))
+      (with-out-str (welcome))))
+
+  (it "Prints the board"
+    (should= " A1  | A2  | A3  \n-----------------\n B1  | B2  | B3  \n-----------------\n C1  | C2  | C3  \n"
+      (with-out-str (print-board)))))
+
+
--- src/tic_tac_toe/cli.clj
+++ src/tic_tac_toe/cli.clj
@@ -2,3 +2,10 @@
 
 (defn welcome []
   (println "Tic Tac Toe"))
+
+(defn print-board []
+  (println (str " A1  | A2  | A3  \n"
+                "-----------------\n"
+                " B1  | B2  | B3  \n"
+                "-----------------\n"
+                " C1  | C2  | C3  ")))
