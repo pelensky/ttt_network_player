556839984179cc760317274d8d5eb2a763783b41
Dan Pelensky <dan@pelensky.com>
2017-07-13 16:10:08 +0100

    updated to use a map of letters rather than numbers

--- src/tic_tac_toe/input.clj
+++ src/tic_tac_toe/input.clj
@@ -1,7 +1,20 @@
-(ns tic-tac-toe.input)
+(ns tic-tac-toe.input
+  (:require [clojure.string :as string]))
+
+
+(def conversions
+  { "A1" 0 "A2" 1 "A3" 2
+    "B1" 3 "B2" 4 "B3" 5
+    "C1" 6 "C2" 7 "C3" 8})
 
 (defn get-input []
   (read-line))
 
 (defn selection []
-  (Integer/valueOf (read-line)))
+  (let [converted-selection (get conversions
+    (string/upper-case
+      (string/trim
+        (read-line))))]
+          (if (not (nil? converted-selection))
+            converted-selection
+            (recur))))
