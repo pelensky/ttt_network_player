18e4ba3459b559a68c15c2481a9799ab1909880f
Dan Pelensky <dan@pelensky.com>
2017-07-13 15:14:06 +0100

    removed declare which is no longer necessary

--- src/tic_tac_toe/app_runner.clj
+++ src/tic_tac_toe/app_runner.clj
@@ -3,10 +3,9 @@
             [tic-tac-toe.input :as ttt-input]
             [tic-tac-toe.output :as ttt-output]))
 
-(declare game-runner)
-(declare single-turn)
-(declare end-of-game)
-
+(defn end-of-game [board]
+  (ttt-output/print-board board)
+  (ttt-output/print-game-over))
 
 (defn game-runner [board]
   (ttt-output/print-take-turn board)
@@ -18,10 +17,6 @@
 
 (defn single-turn [board]
   (ttt-board/take-turn (ttt-input/selection) board))
-
-(defn end-of-game [board]
-  (ttt-output/print-board board)
-  (ttt-output/print-game-over))
 
 (defn play []
   (ttt-output/print-welcome)
