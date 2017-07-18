b2aba417119238daee57d7327c6168fd02c8ad01
Dan Pelensky <dan@pelensky.com>
2017-07-18 17:48:16 +0100

    random computer selects a random available space

--- src/tic_tac_toe/random_computer.clj
+++ src/tic_tac_toe/random_computer.clj
@@ -1,1 +1,11 @@
 (ns tic-tac-toe.random-computer)
+
+(defn get-available-spaces [board]
+  (remove nil? (vec
+    (for [space (range 9)]
+      (if (not (some (partial = space) board)) space)))))
+
+(defn choose-space [board]
+  (rand-nth (get-available-spaces board)))
+
+
