48e5148e49a13a53b2bc4663dd3fe18cfbeca009
Dan Pelensky <dan@pelensky.com>
2017-07-18 15:50:25 +0100

    cli set up to get players

--- src/tic_tac_toe/app_runner.clj
+++ src/tic_tac_toe/app_runner.clj
@@ -1,5 +1,6 @@
 (ns tic-tac-toe.app-runner
   (:require [tic-tac-toe.board :as ttt-board]
+            [tic-tac-toe.player-type :as player-type]
             [tic-tac-toe.input :as input]
             [tic-tac-toe.output :as output]))
 
@@ -8,16 +9,23 @@
   (output/print-message (output/game-over board)))
 
 (defn single-turn [board]
-  (ttt-board/take-turn (input/selection) board))
+  (ttt-board/take-turn (input/space-selection) board))
 
-(defn game-runner [board]
+(defn game-runner [board players]
   (output/print-message (output/take-turn board))
   (output/print-message (output/format-board board))
   (let [updated-board (single-turn board)]
     (if (ttt-board/game-over? updated-board)
       (end-of-game updated-board)
-      (recur updated-board))))
+      (recur updated-board players))))
+
+(defn select-players [players]
+  (output/print-message (output/player-type (if (empty? players) "X" "O")))
+  (let [updated-players (player-type/select-players players (player-type/select-player (input/get-player)))]
+    (if (= 2 (count updated-players ))
+      (game-runner [] updated-players)
+      (recur updated-players))))
 
 (defn play []
   (output/print-message (output/welcome))
-  (game-runner []))
+  (select-players []))
