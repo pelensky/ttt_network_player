1ddba1f8e2eb27b47adc7a712bc139104c7686bd
Dan Pelensky <dan@pelensky.com>
2017-07-18 14:05:45 +0100

    removed front ttt from ttt-input and ttt-output namespaces

--- src/tic_tac_toe/app_runner.clj
+++ src/tic_tac_toe/app_runner.clj
@@ -1,23 +1,23 @@
 (ns tic-tac-toe.app-runner
   (:require [tic-tac-toe.board :as ttt-board]
-            [tic-tac-toe.input :as ttt-input]
-            [tic-tac-toe.output :as ttt-output]))
+            [tic-tac-toe.input :as input]
+            [tic-tac-toe.output :as output]))
 
 (defn end-of-game [board]
-  (ttt-output/print-message (ttt-output/format-board board))
-  (ttt-output/print-game-over board))
+  (output/print-message (output/format-board board))
+  (output/print-game-over board))
 
 (defn single-turn [board]
-  (ttt-board/take-turn (ttt-input/selection) board))
+  (ttt-board/take-turn (input/selection) board))
 
 (defn game-runner [board]
-  (ttt-output/print-message (ttt-output/take-turn board))
-  (ttt-output/print-message (ttt-output/format-board board))
+  (output/print-message (output/take-turn board))
+  (output/print-message (output/format-board board))
   (let [updated-board (single-turn board)]
     (if (ttt-board/game-over? updated-board)
       (end-of-game updated-board)
       (recur updated-board))))
 
 (defn play []
-  (ttt-output/print-message (ttt-output/welcome))
+  (output/print-message (output/welcome))
   (game-runner []))
