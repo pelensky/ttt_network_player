f67cb20561209a98f26a6708ce0657b6832c25ac
Dan Pelensky <dan@pelensky.com>
2017-07-19 14:56:36 +0100

    renamed get-integer method

--- src/tic_tac_toe/input.clj
+++ src/tic_tac_toe/input.clj
@@ -1,7 +1,6 @@
 (ns tic-tac-toe.input
   (:require [clojure.string :as string]))
 
-
 (def conversions
   { "A1" 0 "B1" 1 "C1" 2
     "A2" 3 "B2" 4 "C2" 5
@@ -17,7 +16,7 @@
             converted-selection
             (recur))))
 
-(defn get-integer []
+(defn validate-integer []
   (or (try 
     (Integer/parseInt (read-line))
       (catch Exception e))
@@ -25,7 +24,7 @@
 
 (defn get-number []
   (let [selection 
-    (get-integer)]
+    (validate-integer)]
       (if (or (= selection 1) (= selection 2))
         selection
           (recur))))
