f2f0cd2c388eecfb56ea0452d9cca8b9a5bd9fae
Dan Pelensky <dan@pelensky.com>
2017-07-24 16:16:54 +0100

    changed board data structure to incorporate the size

--- spec/tic_tac_toe/board_spec.clj
+++ spec/tic_tac_toe/board_spec.clj
@@ -5,135 +5,135 @@
 (describe "Taking turns"
           (it "X plays"
               (should= [1]
-                       (place-marker 1 [])))
+                       (place-marker 1 {:size 3 :board[]})))
 
           (it "X play in a different space"
               (should= [0]
-                       (place-marker 0 [])))
+                       (place-marker 0 {:size 3 :board[]})))
 
           (it "O play after X"
               (should= [8 1]
-                       (place-marker 1 [8])))
+                       (place-marker 1 {:size 3 :board [8]} )))
 
           (it "rejects duplicate values"
               (should= [0 1 2]
-                       (place-marker 0 [0 1 2])))
+                       (place-marker 0 {:size 3 :board [0 1 2]})))
 
           (it "check value of empty space"
               (should= nil
-                       (check-value-of-space 0 [])))
+                       (check-value-of-space 0 [] )))
 
           (it "check value of taken space"
               (should= "X"
-                       (check-value-of-space 0 [0])))
+                       (check-value-of-space 0 [0] )))
 
           (it "check value of a space taken by O"
               (should= "O"
-                       (check-value-of-space 5 [0 5]))))
+                       (check-value-of-space 5 [0 5] ))))
 
 (describe "Winning Positions"
           (it "gets all rows"
               (should= [[0 1 2] [3 4 5] [6 7 8]]
-                       (rows)))
+                       (rows 3)))
 
           (it "gets all columns"
               (should= [[0 3 6] [1 4 7] [2 5 8]]
-                       (columns)))
+                       (columns 3)))
 
           (it "gets the diagonals"
               (should= [[0 4 8] [2 4 6]]
-                       (diagonals)))
+                       (diagonals 3)))
 
           (it "combines all the winning combinations"
               (should= [[0 1 2] [3 4 5] [6 7 8] [0 3 6] [1 4 7] [2 5 8] [0 4 8] [2 4 6]]
-                       (winning-positions))))
+                       (winning-positions 3))))
 
 
 (describe "Split Board"
           (it "creates a vector with moves - one"
               (should= ["X" nil nil nil nil nil nil nil nil]
-                       (convert-board-to-full-board [0])))
+                       (convert-board-to-full-board {:size 3 :board [0]})))
 
           (it "creates a vector with moves - multiple"
               (should= ["X" "X" "O" "O" "O" "X" "X" nil nil]
-                       (convert-board-to-full-board [0 4 1 2 6 3 5])))
+                       (convert-board-to-full-board {:size 3 :board [0 4 1 2 6 3 5]})))
 
           (it "splits the vector into three"
               (should= [["X" "X" "O"] ["O" "O" "X"] ["X" nil nil]]
-                       (split-board-into-rows ["X" "X" "O" "O" "O" "X" "X" nil nil])))
+                       (split-board-into-rows {:size 3 :board [0 2 1 3 6 4 5]})))
 
           (it "splits an empty board into three"
               (should= [[nil nil nil] [nil nil nil] [nil nil nil]]
-                       (split-board-into-rows [nil nil nil nil nil nil nil nil nil])))
+                       (split-board-into-rows {:size 3 :board []})))
 
           (it "gets row 1"
               (should-contain ["X" "X" "O"]
-                              (winning-scenarios [0 4 1 2 6 3 5])))
+                              (winning-scenarios {:size 3 :board [0 4 1 2 6 3 5]})))
 
           (it "gets row 2"
               (should-contain ["O" "O" "X"]
-                              (winning-scenarios [0 4 1 2 6 3 5])))
+                              (winning-scenarios {:size 3 :board [0 4 1 2 6 3 5]})))
 
           (it "gets row 3"
               (should-contain ["X" nil nil]
-                              (winning-scenarios [0 4 1 2 6 3 5])))
+                              (winning-scenarios {:size 3 :board [0 4 1 2 6 3 5]})))
 
           (it "gets column 1"
               (should-contain ["X" "O" "X"]
-                              (winning-scenarios [0 4 1 2 6 3 5])))
+                              (winning-scenarios {:size 3 :board [0 4 1 2 6 3 5]})))
 
           (it "gets column 2"
               (should-contain ["X" "O" nil]
-                              (winning-scenarios [0 4 1 2 6 3 5])))
+                              (winning-scenarios {:size 3 :board [0 4 1 2 6 3 5]})))
 
           (it "gets column 3"
               (should-contain ["O" "X" nil]
-                              (winning-scenarios [0 4 1 2 6 3 5])))
+                              (winning-scenarios {:size 3 :board [0 4 1 2 6 3 5]})))
 
           (it "gets the left diagonal"
               (should-contain ["X" "O" nil]
-                              (winning-scenarios [0 4 1 2 6 3 5])))
+                              (winning-scenarios {:size 3 :board [0 4 1 2 6 3 5]})))
 
           (it "gets the right diagonal"
               (should-contain ["O" "O" "X"]
-                              (winning-scenarios [0 4 1 2 6 3 5]))))
+                              (winning-scenarios {:size 3 :board [0 4 1 2 6 3 5]}))))
 
 (describe "End of game"
           (it "is over when there is a win in a row"
               (should= true
-                       (game-won-by? "X" [0 6 1 7 2])))
+                       (game-won-by? "X" {:size 3 :board [0 6 1 7 2]} )))
 
           (it "is over when there is a win in a column"
               (should= true
-                       (game-won-by? "O" [0 4 3 1 2 7])))
+                       (game-won-by? "O" {:size 3 :board [0 4 3 1 2 7]})))
 
           (it "is over when there is a win in the left diagonal"
               (should= true
-                       (game-won-by? "X" [0 1 4 3 8])))
+                       (game-won-by? "X" {:size 3 :board [0 1 4 3 8]})))
 
           (it "is over when there is a win in the right diagonal"
               (should= true
-                       (game-won-by? "O" [0 2 3 4 1 6])))
+                       (game-won-by? "O" {:size 3 :board [0 2 3 4 1 6]})))
 
           (it "is not won when there are no moves"
               (should= false
-                       (game-won-by? "X" [])))
+                       (game-won-by? "X" {:size 3 :board []})))
 
           (it "is not won when the game is tied"
               (should= false
-                       (game-won-by? "O" [0 1 2 3 4 6 5 7 8])))
+                       (game-won-by? "O" {:size 3 :board [0 1 2 3 4 6 5 7 8]})))
 
           (it "is not won when the game is won by the other player"
               (should= false
-                       (game-won-by? "X" [0 2 3 4 1 6])))
+                       (game-won-by? "X" {:size 3 :board [0 2 3 4 1 6]})))
 
           (it "no winner 1"
               (should= false
-                       (game-won-by? "X" [0 1 2 3 4 6 7 8 5])))
+                       (game-won-by? "X" {:size 3 :board [0 1 2 3 4 6 7 8 5]} )))
 
           (it "no winner 2"
               (should= false
-                       (game-won-by? "O" [0 1 2 3 4 6 7 8 5])))
+                       (game-won-by? "O" {:size 3 :board [0 1 2 3 4 6 7 8 5]})))
 
           (it "checks all elements in a vector equal what it is checking"
               (should= true
@@ -145,37 +145,37 @@
 
           (it "is tied when the board is full and nobody has won"
               (should= true
-                       (game-tied? [0 1 2 3 4 6 7 8 5])))
+                       (game-tied? {:size 3 :board [0 1 2 3 4 6 7 8 5]})))
 
           (it "is tied when the board is full"
               (should= true
-                       (game-tied? [0 1 2 3 4 8 5 6 7])))
+                       (game-tied? {:size 3 :board [0 1 2 3 4 8 5 6 7]})))
 
           (it "is not tied when the board is full but the game is won by X"
               (should= false
-                       (game-tied? [0 1 2 3 4 5 7 8 6])))
+                       (game-tied? {:size 3 :board [0 1 2 3 4 5 7 8 6]})))
 
           (it "is not tied when the board is full but the game is won by X"
               (should= false
-                       (game-tied? [0 1 2 4 3 6 5 7])))
+                       (game-tied? {:size 3 :board [0 1 2 4 3 6 5 7]})))
 
           (it "isn't tied when the board has no moves"
               (should= false
-                       (game-tied? [])))
+                       (game-tied? {:size 3 :board []})))
 
           (it "is over when the game is won by X"
               (should= true
-                       (game-over? [0 6 1 7 2])))
+                       (game-over? {:size 3 :board [0 6 1 7 2]})))
 
           (it "is not over when there has been no moves"
               (should= false
-                       (game-over? [])))
+                       (game-over? {:size 3 :board []})))
 
           (it "is over when the game is won by O"
               (should= true
-                       (game-over? [0 4 3 1 2 7])))
+                       (game-over? {:size 3 :board [0 4 3 1 2 7]} )))
 
           (it "is over when the game is tied"
               (should= true
-                       (game-over? [0 1 2 3 4 6 5 8 7]))))
+                       (game-over? {:size 3 :board [0 1 2 3 4 6 5 8 7]}))))
 
