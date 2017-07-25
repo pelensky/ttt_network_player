15cf9ab46c421db7baf0938bd4dac60bdc473a7b
Dan Pelensky <dan@pelensky.com>
2017-07-25 11:05:18 +0100

    computer knows it's marker

--- spec/tic_tac_toe/unbeatable_computer_spec.clj
+++ spec/tic_tac_toe/unbeatable_computer_spec.clj
@@ -6,4 +6,11 @@
 
           (it "chooses the only available space"
               (should= 7
-                       (choose-space { :size 3 :board [0 1 2 3 4 8 5 6]}))))
+                       (choose-space { :size 3 :board [0 1 2 3 4 8 5 6]})))
+
+          (it "finds the marker if X"
+              (should= "X"
+                       (find-computer-marker {:size 3 :board []})))
+          (it "finds the marker if O"
+              (should= "O"
+                      (find-computer-marker {:size 3 :board [0]}))))
--- src/tic_tac_toe/unbeatable_computer.clj
+++ src/tic_tac_toe/unbeatable_computer.clj
@@ -1,7 +1,13 @@
-(ns tic-tac-toe.unbeatable-computer)
+(ns tic-tac-toe.unbeatable-computer
+  (:require [tic-tac-toe.board :as ttt-board]))
 
-( defn minimax [board-state depth best-score]
+( defn minimax [board-state depth best-score marker]
    7 )
 
+(defn find-computer-marker [board-state]
+  (if ( #(even? ( count %)) (get board-state :board))
+    "X"
+    "O"))
+
 (defn choose-space [board-state]
-  (minimax board-state 0 {}))
+  (minimax board-state 0 {} (find-computer-marker board-state)))
