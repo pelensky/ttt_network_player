8abbb61ba473aacd9eb220bd5adcd02ce8b03e92
Dan Pelensky <dan@pelensky.com>
2017-07-26 16:19:05 +0100

    removed best-score which was not being used

--- src/tic_tac_toe/unbeatable_computer.clj
+++ src/tic_tac_toe/unbeatable_computer.clj
@@ -23,19 +23,19 @@
 (defn top-score [board-state best-score marker]
   (val (best-space-and-score board-state best-score marker)))
 
-(defn update-best-score [board-state depth color best-score marker]
+(defn update-best-score [board-state depth color marker]
    (for [space (ttt-board/find-available-spaces board-state)]
-     (let [negamax-score (* -1 (negamax (ttt-board/place-marker space board-state) (inc depth) (* -1 color) best-score marker))]
-      (assoc best-score space (max -1000 negamax-score) ))))
+     (let [negamax-score (* -1 (negamax (ttt-board/place-marker space board-state) (inc depth) (* -1 color)  marker))]
+      { space (max -1000 negamax-score)} )))
 
-(defn negamax [board-state depth color best-score marker]
+(defn negamax [board-state depth color marker]
     (if (ttt-board/game-over? board-state)
       (* color (score-scenarios board-state depth marker))
       (do
-        (let [updated-best-score (update-best-score board-state depth color best-score marker)]
+        (let [scores (update-best-score board-state depth color marker)]
           (if (= depth 0)
-            (best-space board-state updated-best-score marker)
-            (top-score board-state updated-best-score marker))))))
+            (best-space board-state scores marker)
+            (top-score board-state scores marker))))))
 
 (defn choose-space [board-state]
-  (negamax board-state 0  1 (hash-map) (find-computer-marker board-state)))
+  (negamax board-state 0  1 (find-computer-marker board-state)))
